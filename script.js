fcad9dee2bd636ccbaeb44392a04d07705e76a80
MikhailIK <raxmet777@yandex.ru>
2023-01-31 18:17:08 +0300

    fixed errors

--- script.js
+++ script.js
@@ -41,44 +41,27 @@ let monthNumber = 12;
 
 switch (monthNumber) {
     case 12:
-        console.log("Зима");
-        break;
     case 1:
-        console.log("Зима");
-        break;
     case 2:
         console.log("Зима");
     break;
     case 3:
-        console.log("Зима");
-    break;
     case 4:
-        console.log("Весна");
-    break;
     case 5:
         console.log("Весна");
     break;
     case 6:
-        console.log("Лето");
-    break;
     case 7:
-        console.log("Лето");
-    break;
     case 8:
         console.log("Лето");
     break;
     case 9:
-        console.log("Осень");
-    break;
     case 10:
-        console.log("Осень");
-    break;
     case 11:
         console.log("Осень");
     break;
     default:
         console.log("Неправильно указан номер месяца");
-        break;
 }
 
 
